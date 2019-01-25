f6f2cb5d3bb03986ce51db003be26cbd5ec5cc55
charley mcgowan <charley.mcgowan@outlook.com>
2019-01-25 15:55:34 -0800

    installed bootstrap theme

--- src/app/right-menu/right-menu.component.ts
+++ src/app/right-menu/right-menu.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-right-menu',
   templateUrl: './right-menu.component.html',
   styleUrls: ['./right-menu.component.css']
 })
-export class RightMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class RightMenuComponent {
 
 }
