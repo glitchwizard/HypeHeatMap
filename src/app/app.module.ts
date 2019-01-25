d4ee1366a1f3dfd5c89b12098d1ce5b563e7cd3b
charley mcgowan <charley.mcgowan@outlook.com>
2019-01-25 15:18:13 -0800

    going to try API stuff later, going back to functionality cloning. Adding right menu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { ShowListingComponent } from './show-listing/show-listing.component';
+import { RightMenuComponent } from './right-menu/right-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SiteHeaderComponent,
-    ShowListingComponent
+    ShowListingComponent,
+    RightMenuComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
