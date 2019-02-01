ff98dd93d0bd9e6ab5bdee9f95374f8792dba6ed
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-01 10:21:44 -0800

    Show lists now showing bands

--- src/app/show-listing/show-listing.component.ts
+++ src/app/show-listing/show-listing.component.ts
@@ -1,24 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input , OnInit} from '@angular/core';
 import { Band } from '../models/band.model';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { ShowListingService } from '../show-listing.service';
 
 @Component({
   selector: 'app-show-listing',
   templateUrl: './show-listing.component.html',
-  styleUrls: ['./show-listing.component.css']
+  styleUrls: ['./show-listing.component.css'],
+  providers: [ShowListingService]
 })
-export class ShowListingComponent {
-@Input() childBandList: Band[];
+export class ShowListingComponent implements OnInit {
+  @Input() childBandList: Band[];
+  bandList: FirebaseListObservable<any[]>;
 
-  blockColorArray = [];
+  constructor(private showListingService: ShowListingService) { }
+
+  ngOnInit() {
+    this.bandList = this.showListingService.getBands();
+  }
 
   getRandomHexColor() {
     const randomColorHex = Math.floor(Math.random() * 16777215).toString(16);
     return ('#' + randomColorHex);
   }
-
-  buildBlockColorArray() {
-    this.blockColorArray.push(this.getRandomHexColor());
-    return 'fun';
-  }
-
 }
