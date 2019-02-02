5896d15f05aac40a7956c442eac19680ada84a3b
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-01 16:59:47 -0800

    working on getting admin to work. Data not coming through though

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,15 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AdminComponent } from './admin/admin.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: MainPageComponent
+  },  {
+    path: 'admin',
+    component: AdminComponent
   }
 ];
 
