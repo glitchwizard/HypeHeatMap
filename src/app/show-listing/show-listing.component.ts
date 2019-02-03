7f1b600379e8bf0cc201b28f9ed669aabbe77812
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-02 17:20:45 -0800

    can't figure out how to get observables to work.

--- src/app/show-listing/show-listing.component.ts
+++ src/app/show-listing/show-listing.component.ts
@@ -10,20 +10,36 @@ import { Show } from '../models/show.model';
   providers: [ShowService]
 })
 export class ShowListingComponent implements OnInit {
-  showList: FirebaseListObservable<any[]>;
-  showListWithObjects: Show[];
+  showList: Show[];
+  isBandListPopulated = null;
 
 
-  constructor(private ShowService: ShowService) { }
+  constructor(private ShowService: ShowService) {
+    
+   }
 
   ngOnInit() {
-    this.showList = this.ShowService.getShows();
-    
-    this.ShowService.getShows().subscribe(dataLastEmittedFromObserver => { this.showListWithObjects = dataLastEmittedFromObserver;
+    console.log('something');
+
+        this.ShowService.getShows().subscribe(dataLastEmittedFromObserver => { this.showList = dataLastEmittedFromObserver;
+        console.log(this.showList);
     });
+    debugger;
+    this.checkBandList();
+    console.log("test123");
+    console.log(this.isBandListPopulated);
+
+    //WIP
+    /*
+    the goal right now is to try to get this thing to check to see whether the band list has anything in it for a show, and if it does, return true if the list is greater than zero items. It keeps returning as 'undefined' and I can't figure out how to get it to populate and THEN check.
+    */
   }
 
-
+  checkBandList() {
+    if (this.showList.length | async > 0) {
+      this.isBandListPopulated = 'yes';
+    }
+  }
 
   getRandomHexColor() {
     const randomColorHex = Math.floor(Math.random() * 16777215).toString(16);
